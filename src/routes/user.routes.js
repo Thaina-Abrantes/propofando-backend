fbd07f3cccc81c558879419a5653abef18e8851b
Vanessa da Silva Lima de Carvalho <vanessa.carvalho@residencia.dev>
2022-03-10 14:41:03 -0300

    example validation with yup

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,10 +1,27 @@
 const { Router } = require('express');
 
-const { getUsers, createUser } = require('../controllers/user');
+const { 
+    getUsers, 
+    createUser,
+ } = require('../controllers/user');
+
+const {
+    validateBody,
+  } = require('../middlewares/validateRequest');
+
+const { createUserSchema } = require('../validations/userSchema'); 
 
 const routes = Router();
 
-routes.get("/users", getUsers);
-routes.post("/users", createUser);
+routes.get(
+    '/users',
+     getUsers,
+);
+
+routes.post(
+    '/users',
+    validateBody(createUserSchema),
+    createUser,
+);
 
 module.exports = routes;
---
+++ src/validations/userSchema.js
@@ -0,0 +1,20 @@
+const yup = require('yup');
+
+const createUserSchema = yup.object().shape({
+  firstName: yup
+    .string()
+    .strict()
+    .required(),
+
+  lastName: yup
+    .string()
+    .strict()
+    .required(),
+  age: yup
+    .number()
+    .required()
+});
+
+module.exports = {
+  createUserSchema,
+};
