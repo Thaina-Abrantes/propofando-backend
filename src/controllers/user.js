8dc952dee5799fcc491f8e2002b4b398f01fc666
Thaina-Abrantes <oliveiraabrantess@gmail.com>
2022-03-13 13:32:34 -0300

    Criptografia de senha

--- src/controllers/user.js
+++ src/controllers/user.js
@@ -1,5 +1,6 @@
-const { UserRepository } = require("../repositories/UserRepository")
-const { validateUser } = require("../helpers/validators/userValidator")
+const { UserRepository } = require("../repositories/UserRepository");
+const { validateUser } = require("../helpers/validators/userValidator");
+const bcrypt = require('bcrypt');
 
 const userRepository = new UserRepository()
 
@@ -11,19 +12,22 @@ async function getUsers(_, response) {
 
 async function createUser(request, response) {
     const { fullName, email, password } = request.body;
-    
+
     const indexPassword = email.indexOf("@");
     const defaultPassword = email.substring(0, indexPassword + 1);
 
     const userWithRegisteredEmail = await (await userRepository.findAll()).find(user => user.email === email);
-    
-    if(userWithRegisteredEmail) {
-        return response.status(400).json({mensagem: "Email já cadastrado."});
-    }
-    
-    const insertedUser = await userRepository.insert({ fullName, email, password: defaultPassword });
 
-    return response.status(201).json({mensagem: "Usuário cadastrado com sucesso." + {insertedUser}});
+    if (userWithRegisteredEmail) {
+        return response.status(400).json({ mensagem: "Email já cadastrado." });
+    }
+
+    const salt = bcrypt.genSaltSync(10);
+    const hash = bcrypt.hashSync(defaultPassword, salt);
+
+    const insertedUser = await userRepository.insert({ fullName, email, password: hash });
+
+    return response.status(201).json({ mensagem: "Usuário cadastrado com sucesso." + { insertedUser } });
 }
 
 module.exports = { getUsers, createUser }
