36e20bb238542b4c99ad0f7464177076a4b26f66
Vanessa da Silva Lima de Carvalho <vanessa.carvalho@residencia.dev>
2022-03-14 09:47:15 -0300

    verify duplicated users

--- src/helpers/validators/helper.js
+++ src/helpers/validators/helper.js
@@ -1,5 +1,39 @@
+const { UserRepository } = require('../../repositories/UserRepository');
+
+const userRepository = new UserRepository();
+
+
 function joiErrorToJavaScriptError(error) {
     return Error(error.message.replace(/"/g, "'"))
 }
 
-module.exports = { joiErrorToJavaScriptError }
+const err = {
+    success: true,
+    message: '',
+  };
+  
+function generateError(success, message) {
+const erro = {
+    success,
+    message,
+};
+
+return erro;
+}
+
+async function verifyDuplicatedUsers(email) {
+    let error = err;
+  
+    const registeredUser = await userRepository.findOneBy({ email });
+  
+    if (registeredUser) {
+      error = generateError(false, 'Informe um email diferente.');
+    }
+    return error;
+}
+
+
+module.exports = { 
+    joiErrorToJavaScriptError, 
+    verifyDuplicatedUsers,
+}
