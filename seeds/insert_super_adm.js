42885145de785f29cebdafc9cdda31d2f517402e
Vanessa da Silva Lima de Carvalho <vanessa.carvalho@residencia.dev>
2022-03-14 09:42:17 -0300

    insert super adm

---
+++ seeds/insert_super_adm.js
@@ -0,0 +1,16 @@
+const { encryptPassword } = require('../src/helpers/handlePassword');
+
+exports.seed = async function (knex) {
+  const encryptedPassword = await encryptPassword(process.env.SUPER_ADMIN_PWD);
+
+  const superAdmin = {
+    id: process.env.SUPER_ADMIN_ID,
+    name: process.env.SUPER_ADMIN_NAME,
+    email: process.env.SUPER_ADMIN_EMAIL,
+    password: encryptedPassword,
+    userType: process.env.SUPER_ADMIN_USERTYPE, 
+  };
+
+  await knex('users').insert(superAdmin);
+
+};
