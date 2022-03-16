8c48f37579a4914c3a25c3f995345e623fa937fd
Vanessa da Silva Lima de Carvalho <vanessa.carvalho@residencia.dev>
2022-03-16 10:05:43 -0300

    alterações na seed que insere o admin

--- src/seeds/20220314134649_insert_super_admin.js
+++ src/seeds/20220314134649_insert_super_admin.js
@@ -1,16 +1,29 @@
+const { generateUuid } = require('../helpers/handleUuid');
 const { encryptPassword } = require('../helpers/handlePassword');
+
+const { UserRepository } = require('../repositories/UserRepository');
+
+const userRepository = new UserRepository();
 
 exports.seed = async function (knex) {
   const encryptedPassword = await encryptPassword(process.env.SUPER_ADMIN_PWD);
 
   const superAdmin = {
-    id: process.env.SUPER_ADMIN_ID,
+    id: generateUuid(),
     name: process.env.SUPER_ADMIN_NAME,
     email: process.env.SUPER_ADMIN_EMAIL,
     password: encryptedPassword,
     userType: process.env.SUPER_ADMIN_USERTYPE, 
   };
 
-  await knex('users').insert(superAdmin);
+  const { name, email, password, userType } = superAdmin;
+
+  const registeredSuperAdmin = await userRepository.findOneBy({userType});
+
+  if (!registeredSuperAdmin) {
+    return await userRepository.insert(superAdmin);
+  }
+
+  await userRepository.update({id: registeredSuperAdmin.id, name, email, password});
 
 };
