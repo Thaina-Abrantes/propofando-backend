7ec881def29a5a35654038840a7c091f8ce9a934
Vanessa da Silva Lima de Carvalho <vanessa.carvalho@residencia.dev>
2022-03-21 17:40:27 -0300

    create question + create alternatives

--- src/controllers/question.js
+++ src/controllers/question.js
@@ -1,16 +1,41 @@
 const { QuestionRepository } = require('../repositories/QuestionRepository');
+const { AlternativeRepository } = require('../repositories/AlternativeRepository');
 
 const questionRepository = new QuestionRepository();
+const alternativeRepository = new AlternativeRepository();
 
+const { generateTransaction } = require('../helpers/handleTransaction');
 
 async function createQuestion(request, response) {
     const { 
         title,
         description, 
-        category,       
+        category,
+        image,
+        explanationVideo,
+        explanationText, 
+        alternatives,      
     } = request.body;
 
-    await questionRepository.insert({ title, description, category});
+    const transaction = await generateTransaction();
+
+    const registeredQuestion = await questionRepository
+        .withTransaction(transaction)
+        .insert({ title, description, category, image, explanationVideo, explanationText});
+
+    for (const alternative of alternatives) {
+        alternative.questionId = registeredQuestion.id;
+    }
+
+    const registeredAlternatives = await alternativeRepository
+        .withTransaction(transaction)
+        .insertAll(alternatives);
+   
+    if(!registeredQuestion || !registeredAlternatives){
+        return response.status(400).json({ message: 'Não foi possível cadastrar a questão.' });
+    }
+
+    transaction.commit();
 
     return response.status(201).json({ message: 'Questão cadastrada com sucesso.' });
 }
