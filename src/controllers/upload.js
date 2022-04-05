fdfde422ffd7a39bdf9f1508219d46fd560d0feb
Vanessa da Silva Lima de Carvalho <vanessa.carvalho@residencia.dev>
2022-04-05 10:24:28 -0300

    controller createGenericUpload

---
+++ src/controllers/upload.js
@@ -0,0 +1,33 @@
+const fsp = require('fs/promises');
+
+const { uploadFile } = require('../helpers/handleUpload');
+
+const removeFileFromDisk = fsp.unlink;
+
+async function createGenericUpload(request, response) {
+  const { file } = request;
+
+  if (!file) {
+    return response.status(400).json('Escolha um arquivo para fazer upload');
+  }
+
+  const { filename, path } = file;
+
+  const readFile = await fsp.readFile(`tmp/uploads/${filename}`, (err, data) => {
+    if (err) {
+      throw err;
+    }
+
+    return data;
+  });
+
+  const upload = await uploadFile(filename, readFile);
+
+  await removeFileFromDisk(path);
+
+  return response.status(200).json({ url: upload });
+}
+
+module.exports = {
+  createGenericUpload,
+};
