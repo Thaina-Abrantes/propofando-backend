02c70025c0eab4ba043966fb66854b5ca1ebf2f2
Vanessa da Silva Lima de Carvalho <vanessa.carvalho@residencia.dev>
2022-04-05 10:25:47 -0300

    routes

--- src/routes/index.js
+++ src/routes/index.js
@@ -8,12 +8,14 @@ const userRoutes = require('./user.routes');
 const loginRoutes = require('./login.routes');
 const categoriesRoutes = require('./category.routes');
 const questionRoutes = require('./question.routes');
+const fileRoutes = require('./upload.routes');
 
 routes.use(userRoutes);
 routes.use(loginRoutes);
 routes.use(categoriesRoutes);
 routes.use(questionRoutes);
+routes.use(fileRoutes);
 
-routes.use(globalErrorHandler)
+routes.use(globalErrorHandler);
 
 module.exports = routes;
---
+++ src/routes/upload.routes.js
@@ -0,0 +1,13 @@
+const { Router } = require('express');
+
+const { createGenericUpload } = require('../controllers/upload');
+
+const authentication = require('../middlewares/authentication');
+
+const { uploadConfig } = require('../../configs/multer.config');
+
+const routes = Router();
+
+routes.post('/upload', authentication, uploadConfig.single('file'), createGenericUpload);
+
+module.exports = routes;
