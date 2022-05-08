963303b8dfcff45cf738df624b0c9c2a51500843
Natália Barbosa <nob.ufba@gmail.com>
2022-05-08 13:13:33 -0300

    consulta para verificar quantas questões de determinada categoria o usuário respondeu e acertou. Falta endpoint

--- src/repositories/CategoryRepository.js
+++ src/repositories/CategoryRepository.js
@@ -1,6 +1,5 @@
-const knexConfig = require('../../knexfile')
-const knex = require('knex')(knexConfig)
-
+const knexConfig = require('../../knexfile');
+const knex = require('knex')(knexConfig);
 
 const { BaseRepository } = require('@cubos/knex-repository');
 
@@ -30,12 +29,34 @@ class CategoryRepository extends BaseRepository {
             .limit(size)
             .offset(page)
             .returning('*');
-    
+
         categories.totalItems = count;
         categories.totalPages = numberOfPages >= 1 ? numberOfPages : 1;
         categories.currentPage = parseInt(pageNumber, 10);
 
         return categories;
+    }
+
+    async getCategoryStatistics() {
+        const categoryStatistics = await knex('users as u')
+        .leftjoin('questions_sort_simulated as qss', 'qss.userId', 'u.id')
+        .leftjoin('questions as q', 'q.id', 'qss.questionId')
+        .leftjoin('category as c', 'c.id', 'q.categoryId')
+        .leftjoin('alternatives as a', 'a.id', 'qss.altenativeId')
+        .select(
+            'u.id',
+            'u.name',
+            'c.name',
+            knex.raw('count(*) filter(where qss.answered = true ) as answeredPerCategory'),
+            knex.raw('count(*) filter (where a.correct = true and a.id = qss."altenativeId" and qss.answered = true) as answeredCorretlyPerCategory'),
+        )
+        .where({
+            'u.active': true,
+            'u.userType': 'student',
+        })
+        .groupBy('c.id', 'u.id');
+
+       return categoryStatistics;
     }
 }
 module.exports = { CategoryRepository };
