2428659706716ca6c8a88cd3ac409ff816af8d2e
Vanessa da Silva Lima de Carvalho <vanessa.carvalho@residencia.dev>
2022-05-20 10:50:46 +0000

    Update user.routes.js

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -75,10 +75,9 @@ routes.get(
 routes.get(
     '/users/:id/performance',
     authentication,
-    validateAccessPermission(['super admin','student']),
+    validateAccessPermission(['super admin', 'student']),
     validateParams(validateUuidSchema),
     performanceUser,
-
 );
 
 routes.post(
